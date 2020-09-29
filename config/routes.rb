adc30a82bb7da337dc6c546ddd9673253d9ec4dc
Brendon Henning <brendonhenning@gmail.com>
2020-09-29 13:17:09 -0500

    added events

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get  'login',     to: 'user_sessions#new'
+  post 'create',    to: 'user_sessions#create'
+  get  'logout',    to: 'user_sessions#destroy'
+
+  post 'new_event', to: 'events#create'
+  get  'new_event', to: 'events#new'
+
   resources :users, only: [:new, :create, :show]
-  
-  get 'login', to: 'user_sessions#new'
-  post 'create', to: 'user_sessions#create'
-  get 'logout', to: 'user_sessions#destroy'
+  resources :events, except: [:edit, :update, :delete]
 
   root 'user_sessions#new'
 end
